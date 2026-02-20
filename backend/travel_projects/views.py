484c15272270850c708815c5e3c4f748d37f4de9
BaranovskyiMykola <baranovskiynick@gmail.com>
2026-02-20 18:59:05 +0200

    Swagger

--- backend/travel_projects/views.py
+++ backend/travel_projects/views.py
@@ -5,6 +5,8 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .models import TravelProject, Place, TravelProjectPlace
 
@@ -33,10 +35,142 @@ class TravelProjectViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin,
         
         instance.delete()
 
+    @swagger_auto_schema(
+        operation_summary="List Travel Project",
+        operation_description="List travel project",
+        responses={
+            200: TravelProjectSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Create Travel Project",
+        operation_description="Create travel project",
+        request_body=TravelProjectCreateSerializer,
+        responses={
+            201: TravelProjectCreateSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Retrieve Travel Project",
+        operation_description="Retreive travel project",
+        responses={
+            200: TravelProjectSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Update Travel Project",
+        operation_description="Update travel project",
+        responses={
+            200: TravelProjectSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=TravelProjectCreateSerializer,
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Destroy Travel Project",
+        operation_description="Destroy travel project",
+        responses={
+            204: TravelProjectSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
 class TravelProjectAddPlaceView(GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TravelProjectAddPlaceSerializer
 
+    @swagger_auto_schema(
+        operation_summary="Add Place To Travel Project",
+        operation_description="Add Place To Travel Project",
+        responses={
+            201: TravelProjectAddPlaceSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=TravelProjectAddPlaceSerializer,
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -46,6 +180,51 @@ class TravelProjectAddPlaceView(GenericAPIView):
 class TravelProjectPlaceViewSet(RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = TravelProjectPlaceSerializer
+
+    @swagger_auto_schema(
+        operation_summary="Retrieve Travel Project Place",
+        operation_description="Retrieve Travel Project Place",
+        responses={
+            200: TravelProjectPlaceSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Update Travel Project Place",
+        operation_description="Update Travel Project Place",
+        responses={
+            200: TravelProjectPlaceSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=TravelProjectPlaceSerializer,
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
     
     def get_queryset(self):
         return TravelProjectPlace.objects.filter(project__user=self.request.user)
@@ -54,3 +233,25 @@ class PlaceViewSet(ListModelMixin, GenericViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = PlaceSerializer
     queryset = Place.objects.all()
+
+    @swagger_auto_schema(
+        operation_summary="List Places",
+        operation_description="List Places",
+        responses={
+            200: PlaceSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        tags=["TravelProject"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
--- backend/user/views.py
+++ backend/user/views.py
@@ -4,6 +4,8 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .models import User
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserProfileSerializer
@@ -13,10 +15,53 @@ class UserRegisterViewSet(CreateModelMixin, GenericViewSet):
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
 
+    @swagger_auto_schema(
+        operation_summary="Register User",
+        operation_description="Register User",
+        responses={
+            200: UserRegisterSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=UserRegisterSerializer,
+        tags=["User"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 class UserLoginView(GenericAPIView):
     permission_classes = [AllowAny]
     serializer_class = UserLoginSerializer
 
+    @swagger_auto_schema(
+        operation_summary="Login User",
+        operation_description="Login User",
+        responses={
+            200: UserLoginSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=UserLoginSerializer,
+        tags=["User"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -26,6 +71,26 @@ class UserProfileView(GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserProfileSerializer
 
+    @swagger_auto_schema(
+        operation_summary="User Profile",
+        operation_description="User Profile",
+        responses={
+            200: UserProfileSerializer,
+            401: "Unauthorized - Invalid or missing token",
+        },
+        request_body=UserProfileSerializer,
+        tags=["User"],
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                description="JWT token with Bearer prefix (Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True,
+                example="Bearer your_jwt_token_here",
+            )
+        ],
+    )
     def get(self, request):
         serializer = self.get_serializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
